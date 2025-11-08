fdcef48d433e7a5766b917935609bd5733ed52e2
Swaymaw <swaymaw@gmail.com>
2025-11-08 19:05:03 +0530

    updated .gitignore for node modules

--- backend/src/utils/config.py
+++
@@ -1,8 +0,0 @@
-ENGINE_PATHS: dict[str, str] = {
-    "stockfish": "/Users/swayamsinghal/Downloads/Projects/ChessAnalysisTool/backend/Engines/stockfish/stockfish-macos-m1-apple-silicon",
-    "lc0": "/Users/swayamsinghal/Downloads/Projects/ChessAnalysisTool/backend/Engines/lc0/lc0-v0.32.0-macos_12.6.1",
-}
-
-WEIGHTS_FILE: dict[str, str] = {
-    "maia-2200": "/Users/swayamsinghal/Downloads/Projects/ChessAnalysisTool/backend/Engines/lc0/maia-2200.pb.gz"
-}
