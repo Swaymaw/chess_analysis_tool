311eb6fa30307e4e18ba00fc863c529273452a4c
Swaymaw <swaymaw@gmail.com>
2025-11-10 23:27:06 +0530

    initiated a fastapi server

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.routes.v1_router import api_v1_router
+
+app = FastAPI(title="Chess API")
+app.include_router(api_v1_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # or ["*"] for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def healthcheck():
+    return {"status": "running"}
