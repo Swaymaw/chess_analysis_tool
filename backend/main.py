321a5e9196eac15c53afcb1b2742cb80a8d419eb
Swaymaw <swaymaw@gmail.com>
2025-11-17 01:07:06 +0530

    few changes to make it work in production domain and server

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes.v1_router import api_v1_router
 
-app = FastAPI(title="Chess API")
+app = FastAPI(title="Chess API", root_path="/api")
 app.include_router(api_v1_router)
 app.add_middleware(
     CORSMiddleware,
